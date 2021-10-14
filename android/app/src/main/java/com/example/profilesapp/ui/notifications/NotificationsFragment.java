00628ff085679557fd44d3cf4530cb6d9e0b9f44
Arthur Machado Porto <arthurmachadoporto@gmail.com>
2021-10-14 17:07:29 -0300

    Apresentando a localização atual do usuário no aplicativo em android puro

--- android/app/src/main/java/com/example/profilesapp/ui/notifications/NotificationsFragment.java
+++ android/app/src/main/java/com/example/profilesapp/ui/notifications/NotificationsFragment.java
@@ -1,5 +1,9 @@
 package com.example.profilesapp.ui.notifications;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,13 +12,31 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.profilesapp.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
-public class NotificationsFragment extends Fragment {
+public class NotificationsFragment extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap mGoogleMap;
+    MapView mMapView;
+    View mView;
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     private NotificationsViewModel notificationsViewModel;
 
@@ -22,14 +44,53 @@ public class NotificationsFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         notificationsViewModel =
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+
+        mView = inflater.inflate(R.layout.fragment_notifications, container, false);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
+        return mView;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mMapView =  (MapView) mView.findViewById(R.id.navigation_map);
+        if (mMapView != null) {
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        MapsInitializer.initialize(getContext());
+        mGoogleMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull Task<Location> task) {
+                    Location location = task.getResult();
+
+                    if (location != null) {
+                        googleMap.addMarker(new MarkerOptions().position(
+                                new LatLng(location.getLatitude(), location.getLongitude())).title("Statue of liberty"));
+
+                        CameraPosition userLocation = CameraPosition.builder().target(
+                                new LatLng(location.getLatitude(), location.getLongitude()))
+                                .zoom(16).bearing(0).tilt(45).build();
+                        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(userLocation));
+                    }
+                }
+            });
+
+        }
+        else
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+    }
+
 }
